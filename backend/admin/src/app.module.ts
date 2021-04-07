3966303b336158712b96eec65ce3e54c9d291642
Heedong Kim <ruthetum@gmail.com>
2021-04-07 15:48:21 +0900

    [Backend-admin] 컨트롤러, 모듈 및 DB 설정

--- backend/admin/src/app.module.ts
+++ backend/admin/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'pw',
+      database: 'kanban',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    PostModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/admin/src/main.ts
+++ backend/admin/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // 접두사 설정
   await app.listen(8000);
 }
 bootstrap();
